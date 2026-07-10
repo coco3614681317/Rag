142bb8b9440537ebc49a825d56b87fa73dda294e
coco3614681317 <coco3614681317@gmail.com>
2026-07-10 11:47:01 +0800

    2 commit

--- 3d_rag/src/3d_rag_guide.ps1
+++ 3d_rag/src/3d_rag_guide.ps1
@@ -1,3 +1,4 @@
+#处理本地3D数据
 conda activate D:\3D\3d_rag\.conda_env
 cd D:\3D\3d_rag  #激活conda环境并进入目标文件夹
 
@@ -28,3 +29,18 @@ python src\query_model.py #任意新的GLB跟库内比对
     7d6a14874eed48c2b720f0d1adfe6dd9 similarity: 0.7415152391978678
     8ff7f1f2465347cd8b80c9b206c2781e similarity: 0.42896657997549503
     efd35e7d21ac482688c294e3b6c9f74e similarity: 0.3632399977628492
+
+
+#导入大模型
+cd D:\3D\3d_rag\models
+
+git clone https://github.com/salesforce/ULIP.git
+
+# 1. 确认 ULIP 有 README 文件
+dir README*
+
+# 2. 搜索 "encode_pc" 在哪里定义/使用
+findstr /S /I "encode_pc" *.py
+
+# 3. 搜索 "load_state_dict" 看看怎么加载模型
+findstr /S /I "load_state_dict" *.py
